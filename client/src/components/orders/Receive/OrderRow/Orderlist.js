3e48d5e671ad5e331bd1f1526cbfe33a2b961bef
digbijaya <digbijaya.biswal@gmail.com>
2018-09-03 14:02:01 +0530

    All drop down in one row
    "clothetype","clothequality", "washtype", "color"

--- client/src/components/orders/Receive/OrderRow/Orderlist.js
+++ client/src/components/orders/Receive/OrderRow/Orderlist.js
@@ -8,7 +8,7 @@ export default class Orderlist extends Component {
     var allFieldsPopulated = this.props.allFieldsPopulated;
 
     return (
-      <div>
+      <div class="form-group row">
         {Object.keys(orders).map(orderKey => {
           return (
             <OrderComponent
