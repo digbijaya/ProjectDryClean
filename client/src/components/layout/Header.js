88657dd3d3aadc19feaf1d930958536084203c9c
sahob1 <brajminator@gmail.com>
2018-09-20 20:00:52 +0530

    drawer added with navigation bar. style disrupted

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -3,54 +3,43 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Logo from "./Logo/Logo";
+import DrawerToggle from "./SideDrawer/DrawerToggle/DrawerToggle";
+import SideDrawer from "./SideDrawer/SideDrawer";
+import NavigationItems from "./Navigation/NavigationItems";
+import classes from "./Header.css";
+import Tux from "../hoc/Tux";
 
 class Header extends Component {
-  onLogoutClick(event) {
-    event.preventDefault();
-    this.props.logoutUser();
-  }
+  state = {
+    showSideDrawer: false
+  };
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
   render() {
     const { isAuthenticated, user } = this.props.auth;
 
-    const authLinks = (
-      <ul className="nav navbar-nav navbar-right">
-        <li>
-          <a
-            href=""
-            onClick={this.onLogoutClick.bind(this)}
-            className="nav-link"
-          >
-            <img
-              src=""
-              alt={user ? user.name : ""}
-              style={{ width: "25px", marginRight: "5px" }}
-            />
-            Logout
-          </a>
-        </li>
-      </ul>
-    );
-    const guestLinks = (
-      <ul className="nav navbar-nav navbar-right">
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Signup</Link>
-        </li>
-      </ul>
-    );
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link className="navbar-brand" to="/">
-              Dryclean
-            </Link>
-          </div>
-          <div>{isAuthenticated ? authLinks : guestLinks}</div>
-        </div>
-      </nav>
+      <Tux>
+        <header className={classes.Landing}>
+          <DrawerToggle clicked={this.sideDrawerOpenHandler} />
+
+          <Logo />
+
+          <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+          </nav>
+        </header>
+        <SideDrawer
+          close={this.sideDrawerCloseHandler}
+          status={this.state.showSideDrawer}
+        />
+      </Tux>
     );
   }
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,88 +4,19 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logoutUser } from "../../actions/authActions";
 import SideDrawer from "./SideDrawer/SideDrawer";
+import NavigationItems from "./Navigation/NavigationItems";
+import Logo from "./Logo/Logo";
+import DrawerToggle from "./SideDrawer/DrawerToggle/DrawerToggle";
 import Tux from "../hoc/Tux";
 
 class Landing extends Component {
-  onLogoutClick(event) {
-    event.preventDefault();
-    this.props.logoutUser();
-  }
   render() {
-    const { isAuthenticated } = this.props.auth;
-
-    const authLinks = (
-      <div className="container">
-        <header className="jumbotron">
-          <div className="container">
-            <h1>Collect/Deliver Units</h1>
-            <p>
-              <Link className="btn btn-primary btn-large" to="/orderreceive">
-                Collect
-              </Link>
-            </p>
-            <p>
-              <Link className="btn btn-primary btn-large" to="/orderdeliver">
-                Deliver
-              </Link>
-            </p>
-            <p>
-              <Link className="btn btn-primary btn-large" to="/reports">
-                Reports
-              </Link>
-            </p>
-            <p>
-              <a
-                href=""
-                onClick={this.onLogoutClick.bind(this)}
-                className="btn btn-primary btn-large"
-              >
-                Logout
-              </a>
-            </p>
-            <button
-              id="collect"
-              className="btn btn-primary btn-lg btn-default btn-lock"
-            >
-              Baner
-            </button>
-          </div>
-        </header>
-      </div>
-    );
-    const guestLinks = (
-      <div className="container">
-        <header className="jumbotron">
-          <div className="container">
-            <h1>Login</h1>
-            <p>
-              <Link className="btn btn-primary btn-large" to="/login">
-                Login
-              </Link>
-            </p>
-          </div>
-        </header>
-      </div>
-    );
-
     return (
       <Tux>
-        <SideDrawer />
-        <div>{isAuthenticated ? authLinks : guestLinks}</div>
+        <div>HOME</div>
       </Tux>
     );
   }
 }
 
-Landing.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-export default connect(
-  mapStateToProps,
-  { logoutUser }
-)(Landing);
+export default Landing;
